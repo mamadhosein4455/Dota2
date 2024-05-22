cb4bad618a9577ff088339be667f9e18e989e7d4
mamadhosein4455 <tisssot089@gmail.com>
2024-05-22 18:06:29 +0330

    the class Admin created and complited

--- Dota2.cpp
+++ Dota2.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <utility>
 
+using namespace std; 
+
 
 class UUser {
 private:
@@ -44,6 +46,151 @@ public:
         cout << "Username: " << username << ", Password: " << password << ", Type: " << type << endl;
     }
 };
+
+class Admin : public UUser {
+public:
+    Admin(const string& uname, const string& pwd, int t) : UUser(uname, pwd, t) {}
+
+    void displayAdmin() const {
+        cout << "Admin Username: " << getUsername() << endl;
+        cout << "Admin Password: " << getPassword() << endl;
+        cout << "Admin Type: " << getType() << endl;
+    }
+
+    void createAccount(const string& uname, const string& pwd, int t) {
+        UUser newUser(uname, pwd, t);
+        saveAccountToFile(newUser);
+    }
+
+    void editAccount(const string& uname, const string& pwd, const string& newUname = "", const string& newPwd = "", int newType = -1) {
+        ifstream inFile("accounts.txt");
+        ofstream tempFile("temp.txt");
+        string line;
+        bool found = false;
+
+        if (inFile.is_open() && tempFile.is_open()) {
+            while (getline(inFile, line)) {
+                stringstream ss(line);
+                string fileUname, filePwd;
+                int fileType;
+                ss >> fileUname >> filePwd >> fileType;
+
+                if (fileUname == uname && filePwd == pwd) {
+                    found = true;
+                    // Update the user's information
+                    if (!newUname.empty()) {
+                        fileUname = newUname;
+                    }
+                    if (!newPwd.empty()) {
+                        filePwd = newPwd;
+                    }
+                    if (newType != -1) {
+                        fileType = newType;
+                    }
+                    tempFile << fileUname << " " << filePwd << " " << fileType << endl;
+                }
+                else {
+                    tempFile << line << endl;
+                }
+            }
+            inFile.close();
+            tempFile.close();
+
+            if (found) {
+                // Replace the old file with the new file
+                remove("accounts.txt");
+                rename("temp.txt", "accounts.txt");
+                cout << "Account for " << uname << " has been updated." << endl;
+            }
+            else {
+                remove("temp.txt");
+                cout << "Error: Account for " << uname << " with provided password not found." << endl;
+            }
+        }
+        else {
+            cerr << "Error: Unable to open file for reading or writing." << endl;
+        }
+    }
+
+    void deleteAccount(const string& uname, const string& pwd) {
+        ifstream inFile("accounts.txt");
+        ofstream tempFile("temp.txt");
+        string line;
+        bool found = false;
+
+        if (inFile.is_open() && tempFile.is_open()) {
+            while (getline(inFile, line)) {
+                stringstream ss(line);
+                string fileUname, filePwd;
+                int fileType;
+                ss >> fileUname >> filePwd >> fileType;
+
+                if (fileUname == uname && filePwd == pwd) {
+                    found = true;
+                    // Do not write this account to the temp file, effectively deleting it
+                }
+                else {
+                    tempFile << line << endl;
+                }
+            }
+            inFile.close();
+            tempFile.close();
+
+            if (found) {
+                // Replace the old file with the new file
+                remove("accounts.txt");
+                rename("temp.txt", "accounts.txt");
+                cout << "Account for " << uname << " has been deleted." << endl;
+            }
+            else {
+                remove("temp.txt");
+                cout << "Error: Account for " << uname << " with provided password not found." << endl;
+            }
+        }
+        else {
+            cerr << "Error: Unable to open file for reading or writing." << endl;
+        }
+    }
+
+    void printAccounts() {
+        ifstream inFile("accounts.txt");
+        string line;
+
+        if (inFile.is_open()) {
+            while (getline(inFile, line)) {
+                stringstream ss(line);
+                string fileUname, filePwd;
+                int fileType;
+                ss >> fileUname >> filePwd >> fileType;
+
+                if (fileType == 1) {
+                    cout << "Student   username: " << fileUname << "  password: " << filePwd << endl;
+                }
+                else {
+                    cout << "Professor username: " << fileUname << "  password: " << filePwd << endl;
+                }
+            }
+            inFile.close();
+        }
+        else {
+            cerr << "Error: Unable to open file for reading." << endl;
+        }
+    }
+
+private:
+    void saveAccountToFile(const UUser& user) {
+        ofstream outFile("accounts.txt", ios::app);
+        if (outFile.is_open()) {
+            outFile << user.getUsername() << " " << user.getPassword() << " " << user.getType() << endl;
+            outFile.close();
+            cout << "Account for " << user.getUsername() << " has been saved to file." << endl;
+        }
+        else {
+            cerr << "Unable to open file for writing." << endl;
+        }
+    }
+};
+
 
 
 int main() {
